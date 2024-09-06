d9a6ffcf716f14bcf784c546c2737fb4260ebf33
vaishali1jan <vaishali1jan@gmail.com>
2024-09-06 10:32:51 +0530

    Sep-6-2nd

--- tests/PracticeSites/DataDrivenPracticeSite1.spec.js
+++ tests/PracticeSites/DataDrivenPracticeSite1.spec.js
@@ -6,45 +6,34 @@ const datafile= JSON.parse(JSON.stringify(require("../../Database/mydata.json"))
 
 describe("DDTTests",()=>{
 
-
     test.beforeEach("Navigate @smoke",async({page})=>{
 
         const GotoURl= new practicepage(page);
         await GotoURl.Navigate();
         await expect(page).toHaveTitle("Practice Test Automation WebSite")
-
     })
 
     test("TotalOptions @smoke",async({page})=>{
-
         await expect(page.locator("//div[@class='col-md-3 mb-2']")).toHaveCount(85)
-
     })
+
     test("Login @smoke",async({page})=>{
         const GotoURl= new practicepage(page);
         await GotoURl.GotoOption("Test Login Page")
         await expect(page).toHaveTitle("Test Login Page for Automation Testing Practice")
         await GotoURl.Login(datafile.username,datafile.password)
         await expect(page.locator("#flash")).toHaveText("You logged into a secure area!")
-
     })
 
     test("InvalidLogin@Reg",async({page})=>{
-        
         const GotoURl= new practicepage(page);
         await GotoURl.GotoOption("Test Login Page")
         await GotoURl.Login("user","user")
         await expect(page.locator("#flash")).toHaveText("Your username is invalid!")
-
     })
 
     test.afterEach("pageClose",async({page})=>{
-
         const GotoURl= new practicepage(page);
         await GotoURl.pageClose();
-
     })
-
-    
-
 })
--- tests/pages/practiceSite.js
+++ tests/pages/practiceSite.js
@@ -1,29 +1,29 @@
-exports.practicepage =  class practicepage{
+exports.practicepage = class practicepage {
 
-    constructor(page){
+    constructor(page) {
         this.page = page
-        this.searchBox= "//input[@id='search-input']";
-        this.searchBtn="//button[@id='search-button']";
-        this.username="//input[@id='username']";
-        this.password= "//input[@id='password']";
-        this.loginBtn="button[type='submit']"
+        this.searchBox = "//input[@id='search-input']";
+        this.searchBtn = "//button[@id='search-button']";
+        this.username = "//input[@id='username']";
+        this.password = "//input[@id='password']";
+        this.loginBtn = "button[type='submit']"
     }
-    async Navigate(){
-       await this.page.goto("https://practice.expandtesting.com/")
+    async Navigate() {
+        await this.page.goto("https://practice.expandtesting.com/")
     }
 
-    async GotoOption(option){
-        let Myselector="//a[normalize-space()='"+option+"']";
+    async GotoOption(option) {
+        let Myselector = "//a[normalize-space()='" + option + "']";
         await this.page.locator(Myselector).click();
     }
 
-    async Login(username,password){
+    async Login(username, password) {
         await this.page.locator(this.username).fill(username);
         await this.page.locator(this.password).fill(password);
         await this.page.locator(this.loginBtn).click();
     }
 
-    async pageClose(username,password){
+    async pageClose(username, password) {
         await this.page.close();
     }
 }
